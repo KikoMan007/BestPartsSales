61e3bffc48141238a03a66fd3266070c78c5e76b
MichaelAsavamedhi <asavamedhi@yahoo.com>
2019-05-27 18:55:41 -0700

    End2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,22 @@ import { FlexModalService } from './shared-components/flex-modal/flex-modal.serv
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  aboutText: string;
+
   constructor(private flexModal: FlexModalService) { }
 
   ngOnInit() {
+
+    this.aboutText = 'This application is designed by Michael Asavamedhi. (C) 2018.';
   }
+
+  showModal() {
+    this.flexModal.openDialog('about-modal');
+    console.log("yayaya")
+
+
+  }
+
 
   // Launch flex modal or alert (3pts reduced) showing about text
 
